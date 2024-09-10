1b19b06964b09b4722310b3905cecc4f6124e7b0
Pedro Antônio <pedroaia02@gmail.com>
2024-09-10 14:29:11 -0300

    feat : exception handler

--- src/main/java/com/microservice/account/exception/CustomerAlredyExistentException.java
+++ src/main/java/com/microservice/account/exception/CustomerAlredyExistentException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class CustomerAlredyExistentExceptin extends RuntimeException {
+public class CustomerAlredyExistentException extends RuntimeException {
 
-    public CustomerAlredyExistentExceptin(String message)
+    public CustomerAlredyExistentException(String message)
     {
         super(message);
     }
