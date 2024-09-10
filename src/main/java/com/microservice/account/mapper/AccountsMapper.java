534646c0d5972ade49d74b4bbbcc6703f85ca630
Pedro Antônio <pedroaia02@gmail.com>
2024-09-09 22:11:10 -0300

    feat : add manual mapping

---
+++ src/main/java/com/microservice/account/mapper/AccountsMapper.java
@@ -0,0 +1,24 @@
+package com.microservice.account.mapper;
+
+import com.microservice.account.dto.AccountsDto;
+import com.microservice.account.entity.Accounts;
+
+public class AccountsMapper {
+
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAdress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+
+
+}
